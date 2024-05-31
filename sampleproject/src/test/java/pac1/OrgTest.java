f7f37ba0e2124e501ab29ac8a4a7e70e2590a2bf
HithashreeCS <hithashreecs06@gmail.com>
2024-05-31 11:23:04 +0530

    initial commit

---
+++ sampleproject/src/test/java/pac1/OrgTest.java
@@ -0,0 +1,19 @@
+package pac1;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+
+	@Test
+	public void createOrgTest()
+	{
+		System.out.println("execute createOrgTest");
+	}
+	
+
+	@Test
+	public void modifiedOrgTest()
+	{
+		System.out.println("execute modifiedOrgTest");
+	}
+}
